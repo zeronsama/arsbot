687b701be8c999c87d470911cc72944fcff6dd4e
zhouzerong233 <475817716@qq.com>
2020-09-05 23:41:19 +0800

    json信息储存实现

--- src/main.cpp
+++ src/main.cpp
@@ -6,54 +6,64 @@
 using namespace std;
 using namespace Cyan;
 
+//全局变量初始化
 string Name[25] = { "Kasumi","Arisa","Rimi","Saya","Tae",
 					"Kanon","Hagumi","Kokoro","Michelle","Kaoru",
 					"Eve","Hina","Aya","Chisato","Maya",
 					"Sayo","Rinko","Lisa","Yukina","Ako",
 					"Tomoe","Tsugumi","Himari","Ran","Moca" };
-struct Egg {int num[25] = { 0 };};
-map<QQ_t, Egg> user;//用户抽了几个蛋，暂时存在缓存里，将来存json
-const void ars_egg(MiraiBot& bot)//ars扭蛋功能实现(记得传入bot变量的地址，这里改了好多次)
+
+string SourceAddress = "C:\\Users\\zhou\\source\\repos\\arsbot\\source";//资源路径
+//初始化结束
+
+const void ars_egg(MiraiBot& bot)//ars扭蛋功能实现(记得传入bot变量的地址，这里改了好多次),json实现
 {
 	bot.On<Message>(
 		[&](Message m)
 		{
-			// 只能通过 Message 获得发送者的QQ号码
-			// 如果想要“原路回复”(好友消息私聊回复，群组消息群组回复)，
-			// 请使用 Message 事件的 Reply 方法。
-			//m.Reply("Message事件可处理三种消息:" + m.MessageChain);
 			if (m.GetMessageType() == MessageType::GroupMessage)// 判断是否群组消息
 			{
-				GroupMessage gm = m.ToGroupMessage();
-				// TODO: 针对群组消息的特别处理
+				GroupMessage gm = m.ToGroupMessage();//针对群组消息的特别处理
 				try
 				{
 					string plain = m.MessageChain.GetPlainText();//消息
 					if (plain == "ars扭蛋")
 					{
 						int which = GetRandomInt() % 25;//这里要放一个随机数
-						GroupImage img = bot.UploadGroupImage("C:\\Users\\zhou\\Desktop\\bangdream\\" + Num2Str(which + 1) + ".jpg");//图片路径
-						user[m.Sender].num[which]++;
+						nlohmann::json EggData;
+						fstream EggDataFile(SourceAddress + "\\EggData.json");//读写json
+						EggDataFile >> EggData;
+						EggDataFile.seekg(0,ios::beg);//跳至文件头
+						if(EggData[Num2Str(m.Sender)].empty()||EggData[Num2Str(m.Sender)][Num2Str(which)].empty())//如果成员为空则创建
+						{
+							for (int i = 0; i < 25; i++)
+								EggData[Num2Str(m.Sender)][Num2Str(i)] = 0;
+							EggData[Num2Str(m.Sender)][Num2Str(which)] = 1;
+						}
+						else//如果成员存在则加1
+							EggData[Num2Str(m.Sender)][Num2Str(which)] = EggData[Num2Str(m.Sender)][Num2Str(which)] + 1;
+						EggDataFile << EggData;
+						EggDataFile.close();
+						MiraiImage img = bot.UploadGroupImage(SourceAddress+"\\bangdream\\" + Num2Str(which + 1) + ".jpg");//图片路径
 						cout << "ars扭蛋 响应" << endl;
 						m.Reply(MessageChain().At(m.Sender).Plain("，您扭出的蛋是" + Name[which]).Image(img));
 						return;
 					}
 					if (plain == "ars扭蛋详情")
 					{
-						string msg = "";
-						for (int i = 0; i < 25; ++i)
-						{
-							if (user[m.Sender].num[i] != 0)
-								msg = msg + Name[i] + "数量为" + Num2Str(user[m.Sender].num[i]) + "\n";//暂时存在缓存里，将来存json
-						}
+						string msg = "";//信息流
+						nlohmann::json EggData;
+						ifstream EggDataFile(SourceAddress + "\\EggData.json");//读json
+						EggDataFile >> EggData;
+						EggDataFile.close();
+						for (int i = 0; i < 25; i++)
+							if (EggData[Num2Str(m.Sender)][Num2Str(i)] != 0)
+								msg = msg + Name[i] + "数量为" + Num2Str(EggData[Num2Str(m.Sender)][Num2Str(i)]) + "\n";
 						cout << "ars扭蛋详情 响应" << endl;
 						m.Reply(MessageChain().At(m.Sender).Plain("您的扭蛋详情为：\n" + msg));
 					}
 				}
-				catch (const std::exception& ex)
-				{
-					cout << ex.what() << endl;
-				}
+				catch (const std::exception& ex){cout << ex.what() << endl;}
 			}
 		});
 	return;
